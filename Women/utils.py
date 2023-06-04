d0c12184062ca9530a84fa0f92e752f4bb900444
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2023-06-04 17:12:11 +0200

    Add autharizatoin check for DataMixin class

--- Women/utils.py
+++ Women/utils.py
@@ -3,5 +3,8 @@ menu = ['home', 'women', 'add page', 'about']
 class DataMixin:
     def get_user_context(self, **kwargs): #Form the necessary context by default
         context = kwargs #Create a dictionary from passed parametrs
-        context['menu'] = menu
+        menu_cop = menu.copy()
+        if  not self.request.user.is_authenticated:
+            menu_cop.pop(2)
+        context['menu'] = menu_cop
         return context
